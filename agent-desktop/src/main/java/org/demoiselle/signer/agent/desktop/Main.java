9ae6550a7798f4fb2bc48591baf517c751d0ad5a
Jose Rene Nery Cailleret Campanario <jose.campanario@serpro.gov.br>
2017-03-10 11:19:50 -0300

    Issue #45 - Ao executar a aplicação, antes de qualquer coisa é verificado online a versão do componente.

--- agent-desktop/src/main/java/org/demoiselle/signer/agent/desktop/Main.java
+++ agent-desktop/src/main/java/org/demoiselle/signer/agent/desktop/Main.java
@@ -1,15 +1,51 @@
 package org.demoiselle.signer.agent.desktop;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.BindException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ResourceBundle;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.servlet.ServletException;
 import javax.swing.JOptionPane;
 
 import org.demoiselle.signer.agent.desktop.web.WSServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) throws ServletException {
+		Main.logger.info("Iniciando o Demoiselle Desktop Agent.");
+		String version = ResourceBundle.getBundle("agent-desktop").getString("version");
+		String urlVersionChecker = ResourceBundle.getBundle("agent-desktop").getString("urlVersionChecker");
+		Main.logger.info("Versão: " + version);
+		Main.logger.info("URL Version Checker: " + urlVersionChecker);
+		try {
+			if (Main.hasNewVersion(urlVersionChecker, version)) {
+				JOptionPane.showMessageDialog(null,
+						"Há uma nova versão disponível do Demoiselle Desktop Agent Component.\n" + 
+						"É importante atualizar o aplicativo. Acesso o site e baixe a nova versão.",
+						"Demoiselle Desktop Agent Component", JOptionPane.WARNING_MESSAGE);
+			}
+		} catch (Throwable error) {
+			JOptionPane.showMessageDialog(null, "Não foi possível verificar se a aplicação está atualizada.",
+					"Demoiselle Desktop Agent Component", JOptionPane.WARNING_MESSAGE);
+			System.exit(1);
+		}
+
 		try {
 			WSServer.getInstance();
 		} catch (RuntimeException error) {
@@ -23,9 +59,68 @@ public class Main {
 			System.exit(1);
 		}
 
-		// Up HTTPS server (Deprecated)
-		// WSServerSSL.getInstance();
-
 		new TrayIcon();
 	}
+
+	private static boolean hasNewVersion(String stringURL, String currentVersion) {
+		System.setProperty("jsse.enableSNIExtension", "false");
+		URL url;
+		try {
+			url = new URL(stringURL);
+		} catch (MalformedURLException e) {
+			return false;
+		}
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(X509Certificate[] c, String a) throws CertificateException {
+			}
+
+			public void checkServerTrusted(X509Certificate[] c, String a) throws CertificateException {
+			}
+		} };
+		HostnameVerifier valid = new HostnameVerifier() {
+			public boolean verify(String h, SSLSession s) {
+				return true;
+			}
+		};
+		HttpsURLConnection.setDefaultHostnameVerifier(valid);
+		try {
+			SSLContext sc = SSLContext.getInstance("TLSv1.2");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		} catch (Throwable error) {
+			return false;
+		}
+		URLConnection connection;
+		try {
+			connection = url.openConnection();
+			connection.connect();
+		} catch (IOException e) {
+			return false;
+		}
+		HttpsURLConnection https = (HttpsURLConnection) connection;
+		String content = null;
+		try {
+			Main.logger.info("Verificando a versão mais nova através do link");
+			InputStream is = (InputStream) https.getContent();
+			byte[] readContent = new byte[https.getContentLength()];
+			is.read(readContent);
+			content = new String(readContent);
+			content = content.trim();
+			Main.logger.info("Versão do link: " + content);
+		} catch (IOException e) {
+			return false;
+		}
+		boolean hasNewVersion = ((content != null) && (!content.equalsIgnoreCase(currentVersion)));
+		if (hasNewVersion) {
+			Main.logger.info("Sim. Existe uma versão mais nova.");
+		} else {
+			Main.logger.info("Não existe atualização a ser feita.");
+		}
+		return hasNewVersion;
+	}
+
 }
