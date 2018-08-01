502a06e78cf8b15795cdef7288529a93da603e72
80621732915 <emerson.saito@serpro.gov.br>
2018-08-01 12:24:00 -0300

    #166

--- policy-impl-cades/src/test/java/org/demoiselle/signer/policy/impl/cades/pkcs7/impl/PDFVerify.java
+++ policy-impl-cades/src/test/java/org/demoiselle/signer/policy/impl/cades/pkcs7/impl/PDFVerify.java
@@ -2,19 +2,30 @@ package org.demoiselle.signer.policy.impl.cades.pkcs7.impl;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.MessageDigest;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.cos.COSString;
+import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.PDSignature;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cms.CMSSignedData;
+import org.bouncycastle.cms.SignerInformation;
+import org.bouncycastle.cms.SignerInformationVerifier;
+import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.demoiselle.signer.core.extension.BasicCertificate;
 import org.demoiselle.signer.policy.impl.cades.SignatureInformations;
 import org.junit.Test;
@@ -26,7 +37,7 @@ public class PDFVerify {
 	public void test() {
 		
 	
-			String filePath = "/home/{usuario}/arquivo.pdf";
+			String filePath = "caminho do arquivo";
 			
 			PDDocument document;
 			try {
@@ -88,6 +99,59 @@ public class PDFVerify {
 			assertTrue(false);
 		}
 	}
+	
+	
+	
+
+    public void testValidateSignatureVlidationTestAdbePkcs7Sha1() throws Exception
+    {
+        String filePath = "caminho arquivo";
+        
+        byte[] pdfByte;
+        PDDocument pdfDoc = null;
+        SignerInformationVerifier verifier = null;
+        try
+        {
+            //pdfByte = IOUtils.toByteArray(this.getClass().getResourceAsStream("Teste_AI_Assinado_Assinador_Livre.pdf"));
+            pdfDoc = PDDocument.load(new File(filePath));
+            PDSignature signature = pdfDoc.getSignatureDictionaries().get(0);
+            byte[] signedContentAsBytes = signature.getSignedContent(new FileInputStream(filePath));
+
+            byte[] signatureAsBytes = signature.getContents(new FileInputStream(filePath));
+            
+            CAdESChecker checker = new CAdESChecker();
+            checker.checkDetachedSignature(signedContentAsBytes, signatureAsBytes);
+                        
+            CMSSignedData cms = new CMSSignedData(new ByteArrayInputStream(signatureAsBytes));
+                        
+            SignerInformation signerInfo = (SignerInformation) cms.getSignerInfos().getSigners().iterator().next();
+            @SuppressWarnings("unchecked")
+			X509CertificateHolder cert = (X509CertificateHolder) cms.getCertificates().getMatches(signerInfo.getSID())
+                    .iterator().next();
+            verifier = new JcaSimpleSignerInfoVerifierBuilder().setProvider(new BouncyCastleProvider()).build(cert);
+
+            boolean verifyRt = signerInfo.verify(verifier);
+            System.out.println("Verify result: " + verifyRt);
+
+            
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] calculatedDigest = md.digest(signedContentAsBytes);
+            byte[] signedDigest = (byte[]) cms.getSignedContent().getContent();
+            System.out.println("Document digest equals: " + Arrays.equals(calculatedDigest, signedDigest));
+            
+            
+        		
+			
+        }
+        finally
+        {
+            if (pdfDoc != null)
+            {
+                pdfDoc.close();
+            }
+        }
+    }
+	
 }
 
 
