c322873a03cbf612fe4854b9c92c8d98e3bbf929
Emerson Sachio Saito <emerson.saito@gmail.com>
2020-06-02 10:59:14 -0300

    #240

---
+++ policy-impl-xades/src/test/java/org/demoiselle/signer/policy/impl/xades/SignatureXAdESTest.java
@@ -0,0 +1,24 @@
+package org.demoiselle.signer.policy.impl.xades;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SignatureXAdESTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testSign() {
+		fail("Not yet implemented");
+	}
+
+}
