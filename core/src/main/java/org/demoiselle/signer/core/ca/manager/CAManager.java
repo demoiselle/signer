3293459e24d289f5eb73fd5b61854c1a252b0999
Thiago Laubstein <laubstein@gmail.com>
2021-08-04 10:14:10 -0300

    Fixes #310 - correção em parse de CN

--- core/src/main/java/org/demoiselle/signer/core/ca/manager/CAManager.java
+++ core/src/main/java/org/demoiselle/signer/core/ca/manager/CAManager.java
@@ -391,8 +391,14 @@ public class CAManager {
 	}
 
 	private String getCN(String x500) {
-		int indexCN = x500.indexOf(CN);
-		int indexFirstComa = x500.indexOf(',',indexCN);
-		return x500.substring(indexCN, indexFirstComa);
+        int indexCN = x500.indexOf(CN);
+        if (indexCN >= 0) {
+            int indexComa = x500.indexOf(',', indexCN);
+            if (indexComa < 0) {
+                return x500.substring(indexCN);
+            }
+            return x500.substring(indexCN, indexComa);
+        }
+        return x500;
 	}
 }
