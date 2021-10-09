c09fa330394297ab3a9b6a856cf5637b986e04a7
kyriosdata <qp02nv*$>
2021-10-09 12:53:54 -0300

    Apenas estilo.

--- core/src/main/java/org/demoiselle/signer/core/factory/GenericFactory.java
+++ core/src/main/java/org/demoiselle/signer/core/factory/GenericFactory.java
@@ -44,106 +44,105 @@ import org.demoiselle.signer.core.util.MessagesBundle;
 /**
  * Abstract factory that concentrates the reading of the configurations for
  * all the other specialized factories as well as the class reflection functionalities.
- *
  */
 abstract public class GenericFactory<F> {
 
-    private String className = null;
-    private static MessagesBundle coreMessagesBundle = new MessagesBundle();
+	private String className = null;
+	private static final MessagesBundle coreMessagesBundle = new MessagesBundle();
 
-
-    /**
-     * Main method of the factory.
-     * This method makes classes from the names of classes defined in environment variables.
-     * Such variables are defined by each concrete factory that implements the abstract factory through the getVariableName () method.
-     *  Once the environment variable is read, the value of the variable is stored in the "className" property.
-     *  If the environment variable is not set, a default object is built through the abstract factoryDefault () method.
+	/**
+	 * Main method of the factory.
+	 * This method makes classes from the names of classes defined in environment variables.
+	 * Such variables are defined by each concrete factory that implements the abstract factory through the getVariableName () method.
+	 * Once the environment variable is read, the value of the variable is stored in the "className" property.
+	 * If the environment variable is not set, a default object is built through the abstract factoryDefault () method.
+	 *
 	 * @return Instance of factory
-     */
-    public F factory() {
-        F result = null;
+	 */
+	public F factory() {
+		if (this.className == null) {
+			this.className = this.getContentFromVariables(getVariableName());
+		}
 
-        if (this.className == null) {
-            this.className = this.getContentFromVariables(getVariableName());
-        }
+		F result = null;
+		if (this.className != null && this.className.length() > 0) {
+			result = this.factoryFromClassName(this.className);
+		} else {
+			result = this.factoryDefault();
+		}
 
-        if (this.className != null && this.className.length() > 0) {
-            result = this.factoryFromClassName(this.className);
-        } else {
-            result = this.factoryDefault();
-        }
+		return result;
+	}
 
-        return result;
-    }
-
-    /**
-     * Instantiate an object from the name of your class
+	/**
+	 * Instantiate an object from the name of your class
+	 *
 	 * @param className class name of new instance
 	 * @return new instance
-     */
-    @SuppressWarnings("all")
-    public F factoryFromClassName(String className) {
-        F result = null;
+	 */
+	@SuppressWarnings("all")
+	public F factoryFromClassName(String className) {
+		F result = null;
 
-        Class clazz = null;
-        try {
-            clazz = Class.forName(className);
-        } catch (Throwable error) {
-            throw new SignerException(coreMessagesBundle.getString("error.class.not.exist", className ), error);
-        }
-        if (clazz != null) {
-            try {
-                result = (F) clazz.newInstance();
-            } catch (Throwable error) {
-                throw new SignerException(coreMessagesBundle.getString("error.class.incompatible",clazz.getCanonicalName()), error);
-            }
-        }
+		Class clazz = null;
+		try {
+			clazz = Class.forName(className);
+		} catch (Throwable error) {
+			throw new SignerException(coreMessagesBundle.getString("error.class.not.exist", className), error);
+		}
+		if (clazz != null) {
+			try {
+				result = (F) clazz.newInstance();
+			} catch (Throwable error) {
+				throw new SignerException(coreMessagesBundle.getString("error.class.incompatible", clazz.getCanonicalName()), error);
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    /**
-     * Search the environment variables or JVM variables for a certain value.
-     * The priority is for the environment variables.
-     *
-     * @param key Variable location key
-     * @return the content defined in one of the variables. NULL, if no variable is defined
-     */
-    private String getContentFromVariables(String key) {
-        String content = System.getenv(key);
-        if (content == null) {
-            content = System.getenv(key.toLowerCase());
-        }
-        if (content == null) {
-            content = System.getenv(key.toUpperCase());
-        }
+	/**
+	 * Search the environment variables or JVM variables for a certain value.
+	 * The priority is for the environment variables.
+	 *
+	 * @param key Variable location key
+	 * @return the content defined in one of the variables. NULL, if no variable is defined
+	 */
+	private String getContentFromVariables(String key) {
+		String content = System.getenv(key);
+		if (content == null) {
+			content = System.getenv(key.toLowerCase());
+		}
+		if (content == null) {
+			content = System.getenv(key.toUpperCase());
+		}
 
-        if (content == null) {
-            content = System.getProperty(key);
-        }
-        if (content == null) {
-            content = System.getProperty(key.toLowerCase());
-        }
-        if (content == null) {
-            content = System.getProperty(key.toUpperCase());
-        }
+		if (content == null) {
+			content = System.getProperty(key);
+		}
+		if (content == null) {
+			content = System.getProperty(key.toLowerCase());
+		}
+		if (content == null) {
+			content = System.getProperty(key.toUpperCase());
+		}
 
-        return content;
-    }
+		return content;
+	}
 
-    /**
-     * It forces the concrete class to fabricate an object by default
-     *
-     * @return new instance
-     */
-    public abstract F factoryDefault();
+	/**
+	 * It forces the concrete class to fabricate an object by default
+	 *
+	 * @return new instance
+	 */
+	public abstract F factoryDefault();
 
-    /**
-     * Every concrete factory needs to define which environment variable
-     *  contains the name of the class to be fabricated
-     *
-     * @return variable name
-     */
-    protected abstract String getVariableName();
+	/**
+	 * Every concrete factory needs to define which environment variable
+	 * contains the name of the class to be fabricated
+	 *
+	 * @return variable name
+	 */
+	protected abstract String getVariableName();
 
 }
